befa70a17a68876eb8896d70014454593352b2ec
Mohammad Esfandiari <m.esfandiari@novin.dev>
2023-01-14 14:07:40 +0330

    fix: complete qr code scanner

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -1,5 +1,5 @@
 import { Box, styled } from "@mui/material";
-import { useState } from "react";
+import { memo, useState } from "react";
 import { QrReader } from "react-qr-reader";
 
 const Container = styled(Box)({
@@ -23,6 +23,21 @@ function Camera() {
     <Container>
       <QrReader
         constraints={{ facingMode: "environment" }}
+        ViewFinder={() => (
+          <div
+            style={{
+              top: 0,
+              left: 0,
+              zIndex: 1,
+              boxSizing: "border-box",
+              border: "50px solid rgba(0, 0, 0, 0.3)",
+              boxShadow: "rgba(255, 0, 0, 0.5) 0px 0px 0px 5px inset",
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        )}
         onResult={(result, error) => {
           if (!!result) {
             setResult(result?.getText());
@@ -32,11 +47,12 @@ function Camera() {
             console.error(error);
           }
         }}
-        containerStyle={{ width: "100%" }}
+        containerStyle={{ width: "320px", height: "240px" }}
+        videoStyle={{ objectFit: "cover" }}
       />
       <Result>{result}</Result>
     </Container>
   );
-};
+}
 
-export default Camera;
+export default memo(Camera);
